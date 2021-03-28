08504176ed477b72a2b2028bda9e3673c3f533b5
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-03-28 16:52:39 +0400

    Add project files.

---
+++ mini Paint/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace mini_Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2ComboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.widthTxtBx = new Guna.UI2.WinForms.Guna2TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HeightTxtbx = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.guna2RadioButton1 = new Guna.UI2.WinForms.Guna2RadioButton();
+            this.guna2RadioButton2 = new Guna.UI2.WinForms.Guna2RadioButton();
+            this.SuspendLayout();
+            // 
+            // guna2ComboBox1
+            // 
+            this.guna2ComboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.guna2ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.guna2ComboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2ComboBox1.FocusedState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2ComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.guna2ComboBox1.HoverState.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.ItemHeight = 30;
+            this.guna2ComboBox1.ItemsAppearance.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Location = new System.Drawing.Point(12, 12);
+            this.guna2ComboBox1.Name = "guna2ComboBox1";
+            this.guna2ComboBox1.ShadowDecoration.Parent = this.guna2ComboBox1;
+            this.guna2ComboBox1.Size = new System.Drawing.Size(140, 36);
+            this.guna2ComboBox1.TabIndex = 0;
+            this.guna2ComboBox1.SelectedIndexChanged += new System.EventHandler(this.guna2ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(176, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Width";
+            // 
+            // widthTxtBx
+            // 
+            this.widthTxtBx.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.widthTxtBx.DefaultText = "100";
+            this.widthTxtBx.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.widthTxtBx.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.widthTxtBx.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.widthTxtBx.DisabledState.Parent = this.widthTxtBx;
+            this.widthTxtBx.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.widthTxtBx.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.widthTxtBx.FocusedState.Parent = this.widthTxtBx;
+            this.widthTxtBx.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.widthTxtBx.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.widthTxtBx.HoverState.Parent = this.widthTxtBx;
+            this.widthTxtBx.Location = new System.Drawing.Point(266, 13);
+            this.widthTxtBx.Name = "widthTxtBx";
+            this.widthTxtBx.PasswordChar = '\0';
+            this.widthTxtBx.PlaceholderText = "";
+            this.widthTxtBx.SelectedText = "";
+            this.widthTxtBx.SelectionStart = 3;
+            this.widthTxtBx.ShadowDecoration.Parent = this.widthTxtBx;
+            this.widthTxtBx.Size = new System.Drawing.Size(100, 36);
+            this.widthTxtBx.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(372, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Height";
+            // 
+            // HeightTxtbx
+            // 
+            this.HeightTxtbx.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.HeightTxtbx.DefaultText = "100";
+            this.HeightTxtbx.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.HeightTxtbx.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.HeightTxtbx.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.HeightTxtbx.DisabledState.Parent = this.HeightTxtbx;
+            this.HeightTxtbx.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.HeightTxtbx.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.HeightTxtbx.FocusedState.Parent = this.HeightTxtbx;
+            this.HeightTxtbx.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.HeightTxtbx.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.HeightTxtbx.HoverState.Parent = this.HeightTxtbx;
+            this.HeightTxtbx.Location = new System.Drawing.Point(471, 8);
+            this.HeightTxtbx.Name = "HeightTxtbx";
+            this.HeightTxtbx.PasswordChar = '\0';
+            this.HeightTxtbx.PlaceholderText = "";
+            this.HeightTxtbx.SelectedText = "";
+            this.HeightTxtbx.SelectionStart = 3;
+            this.HeightTxtbx.ShadowDecoration.Parent = this.HeightTxtbx;
+            this.HeightTxtbx.Size = new System.Drawing.Size(100, 36);
+            this.HeightTxtbx.TabIndex = 4;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(662, 12);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(69, 28);
+            this.guna2Button1.TabIndex = 5;
+            this.guna2Button1.Text = "Select";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(577, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Color";
+            // 
+            // guna2RadioButton1
+            // 
+            this.guna2RadioButton1.AutoSize = true;
+            this.guna2RadioButton1.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2RadioButton1.CheckedState.BorderThickness = 0;
+            this.guna2RadioButton1.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2RadioButton1.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2RadioButton1.CheckedState.InnerOffset = -4;
+            this.guna2RadioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.guna2RadioButton1.Location = new System.Drawing.Point(753, 12);
+            this.guna2RadioButton1.Name = "guna2RadioButton1";
+            this.guna2RadioButton1.Size = new System.Drawing.Size(37, 17);
+            this.guna2RadioButton1.TabIndex = 7;
+            this.guna2RadioButton1.Text = "Fill";
+            this.guna2RadioButton1.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2RadioButton1.UncheckedState.BorderThickness = 2;
+            this.guna2RadioButton1.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.guna2RadioButton1.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // guna2RadioButton2
+            // 
+            this.guna2RadioButton2.AutoSize = true;
+            this.guna2RadioButton2.CheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2RadioButton2.CheckedState.BorderThickness = 0;
+            this.guna2RadioButton2.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2RadioButton2.CheckedState.InnerColor = System.Drawing.Color.White;
+            this.guna2RadioButton2.CheckedState.InnerOffset = -4;
+            this.guna2RadioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.guna2RadioButton2.Location = new System.Drawing.Point(753, 35);
+            this.guna2RadioButton2.Name = "guna2RadioButton2";
+            this.guna2RadioButton2.Size = new System.Drawing.Size(54, 17);
+            this.guna2RadioButton2.TabIndex = 8;
+            this.guna2RadioButton2.Text = "Empty";
+            this.guna2RadioButton2.UncheckedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2RadioButton2.UncheckedState.BorderThickness = 2;
+            this.guna2RadioButton2.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.guna2RadioButton2.UncheckedState.InnerColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.guna2RadioButton2);
+            this.Controls.Add(this.guna2RadioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.HeightTxtbx);
+            this.Controls.Add(this.widthTxtBx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guna2ComboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ComboBox guna2ComboBox1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2TextBox widthTxtBx;
+        private System.Windows.Forms.Label label2;
+        private Guna.UI2.WinForms.Guna2TextBox HeightTxtbx;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI2.WinForms.Guna2RadioButton guna2RadioButton1;
+        private Guna.UI2.WinForms.Guna2RadioButton guna2RadioButton2;
+    }
+}
+
---
+++ mini Paint/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mini_Paint
+{
+    public partial class Form1 : Form
+    {
+        public Color FigureColor { get; set; }
+        IFactory FigureFactory { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> figures = new List<string> { "Triangle", "Circle", "Rectangle" };
+            guna2ComboBox1.Items.AddRange(figures.ToArray());
+        }
+
+        private void guna2ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = guna2ComboBox1.SelectedItem.ToString();
+            if (item == "Triangle")
+            {
+                FigureFactory = new TriangleFactory();
+            }
+            else if (item == "Rectangle")
+            {
+                FigureFactory = new RectangleFactory();
+            }
+            else if (item == "Circle")
+            {
+                FigureFactory = new CircleFactory();
+            }
+        }
+        List<IFigure> Figures = new List<IFigure>();
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (FigureFactory.GetFigure() is Rectangle rec)
+            {
+                rec.Color = FigureColor;
+                rec.Point = e.Location;
+                rec.size = new Size(int.Parse(widthTxtBx.Text), int.Parse(HeightTxtbx.Text));
+                rec.isFilled = true;
+
+                Figures.Add(rec);
+            }
+            this.Refresh();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(FigureColor, 3);
+            SolidBrush brush = new SolidBrush(FigureColor);
+            using (var a = e.Graphics)
+            {
+                foreach (var item in Figures)
+                {
+                    if (item is Rectangle rec)
+                    {
+                        if (rec.isFilled)
+                        {
+                            a.FillRectangle(brush, rec.Point.X, rec.Point.Y, rec.size.Width, rec.size.Height);
+                        }
+                        else
+                        {
+
+                            a.DrawRectangle(pen, rec.Point.X, rec.Point.Y, rec.size.Width, rec.size.Height);
+
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            var result = colorDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                FigureColor = colorDialog.Color;
+            }
+        }
+    }
+    interface IFigure
+    {
+        Point Point { get; set; }
+        Size size { get; set; }
+        Color Color { get; set; }
+        bool isFilled { get; set; }
+    }
+    class Rectangle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size size { get; set; }
+        public Color Color { get; set; }
+        public bool isFilled { get; set; }
+    }
+    class Circle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size size { get; set; }
+        public Color Color { get; set; }
+        public bool isFilled { get; set; }
+    }
+    class Triangle : IFigure
+    {
+        public Point Point { get; set; }
+        public Size size { get; set; }
+        public Color Color { get; set; }
+        public bool isFilled { get; set; }
+    }
+    interface IFactory
+    {
+        IFigure GetFigure();
+
+    }
+    class CircleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Circle();
+        }
+    }
+    class TriangleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Triangle();
+        }
+    }
+    class RectangleFactory : IFactory
+    {
+        public IFigure GetFigure()
+        {
+            return new Rectangle();
+        }
+    }
+
+}
